bfd3ce83657b0aef0da317129954f34a6d2df2ea
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2023-06-06 22:09:51 +0700

    Service

---
+++ YogaManagement/YogaManagement.Business/Services/Class.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YogaManagement.Business.Services;
+internal class Class
+{
+}
