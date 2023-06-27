851fb638adb48292b516b507bb0622faafeb50f6
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2023-06-27 23:44:34 +0700

    Update TeacherEnrollmentsController.cs

--- YogaManagement/YogaManagement.Application/Controllers/TeacherEnrollmentsController.cs
+++ YogaManagement/YogaManagement.Application/Controllers/TeacherEnrollmentsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OData.Deltas;
 using Microsoft.AspNetCore.OData.Query;
 using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.EntityFrameworkCore;
 using YogaManagement.Application.Utilities;
 using YogaManagement.Business.Repositories;
 using YogaManagement.Contracts.TeacherEnrollment;
@@ -13,11 +14,18 @@ public class TeacherEnrollmentsController : ODataController
 {
     private readonly IMapper _mapper;
     private readonly TeacherEnrollmentRepository _tcErRepo;
+    private readonly TeacherScheduleRepository _tcScheduleRepo;
+    private readonly ScheduleRepository _scheduleRepo;
 
-    public TeacherEnrollmentsController(IMapper mapper, TeacherEnrollmentRepository tcrepo)
+    public TeacherEnrollmentsController(IMapper mapper,
+        TeacherEnrollmentRepository tcrepo,
+        TeacherScheduleRepository tcScheduleRepo,
+        ScheduleRepository scheduleRepo)
     {
         _mapper = mapper;
         _tcErRepo = tcrepo;
+        _tcScheduleRepo = tcScheduleRepo;
+        _scheduleRepo = scheduleRepo;
     }
 
     [EnableQuery]
@@ -46,9 +54,37 @@ public class TeacherEnrollmentsController : ODataController
             ModelState.Remove("TeacherName");
             ModelState.Remove("ClassName");
             ModelState.ValidateRequest();
+
+            var tcSchedule = _tcScheduleRepo.GetAll()
+                .Include(x => x.TimeSlot)
+                .Where(x => x.TeacherProfileId == createRequest.TeacherProfileId
+                    && !x.IsTaken)
+                .ToList();
+
+            var classSchedule = _scheduleRepo.GetAll()
+                .Include(x => x.TimeSlot)   
+                .Where(x => x.YogaClassId == createRequest.YogaClassId)
+                .ToList().Select(x => x.TimeSlotId);
+
+            foreach (var timeSlot in classSchedule)
+            {
+                if (!tcSchedule.Select(x => x.TimeSlotId).Contains(timeSlot))
+                {
+                    throw new Exception("Teacher dont have available time slot for class");
+                }
+            }
+
             var newTcEnr = _mapper.Map<TeacherEnrollment>(createRequest);
             newTcEnr.IsActive = true;
             await _tcErRepo.CreateAsync(newTcEnr);
+
+            foreach (var timeSlot in classSchedule)
+            {
+                var teacherTimeSlot = tcSchedule
+                    .SingleOrDefault(x => x.TimeSlotId == timeSlot);
+                teacherTimeSlot.IsTaken = true;
+                await _tcScheduleRepo.UpdateAsync(teacherTimeSlot);
+            }
             return Created(createRequest);
         }
         catch (Exception ex)
@@ -57,33 +93,33 @@ public class TeacherEnrollmentsController : ODataController
         }
     }
 
-    public async Task<IActionResult> Patch([FromRoute] int key, [FromBody] Delta<TeacherEnrollmentDTO> delta)
-    {
-        var updateRequest = delta.GetInstance();
-        var existEnroll = await _tcErRepo.Get(key);
-        if (existEnroll == null)
-        {
-            return NotFound();
-        }
-        else
-        {
-            try
-            {
-                //var tcEnroll = _mapper.Map(updateRequest, existEnroll);
-                existEnroll.StartDate = updateRequest.StartDate;
-                existEnroll.EndDate = updateRequest.EndDate;
-                existEnroll.TeacherProfileId = updateRequest.TeacherProfileId;
-                existEnroll.YogaClassId = updateRequest.YogaClassId;
-                existEnroll.IsActive = updateRequest.IsActive;
-                await _tcErRepo.UpdateAsync(existEnroll);
-                return Updated(existEnroll);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
-    }
+    //public async Task<IActionResult> Patch([FromRoute] int key, [FromBody] Delta<TeacherEnrollmentDTO> delta)
+    //{
+    //    var updateRequest = delta.GetInstance();
+    //    var existEnroll = await _tcErRepo.Get(key);
+    //    if (existEnroll == null)
+    //    {
+    //        return NotFound();
+    //    }
+    //    else
+    //    {
+    //        try
+    //        {
+    //            //var tcEnroll = _mapper.Map(updateRequest, existEnroll);
+    //            existEnroll.StartDate = updateRequest.StartDate;
+    //            existEnroll.EndDate = updateRequest.EndDate;
+    //            existEnroll.TeacherProfileId = updateRequest.TeacherProfileId;
+    //            existEnroll.YogaClassId = updateRequest.YogaClassId;
+    //            existEnroll.IsActive = updateRequest.IsActive;
+    //            await _tcErRepo.UpdateAsync(existEnroll);
+    //            return Updated(existEnroll);
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            return BadRequest(ex.Message);
+    //        }
+    //    }
+    //}
 
     public async Task<IActionResult> Delete(int key)
     {
@@ -94,8 +130,27 @@ public class TeacherEnrollmentsController : ODataController
         }
         try
         {
+            var tcSchedule = _tcScheduleRepo.GetAll()
+                .Include(x => x.TimeSlot)
+                .Where(x => x.TeacherProfileId == existEnroll.TeacherProfileId
+                    && !x.IsTaken)
+                .ToList();
+
+            var classSchedule = _scheduleRepo.GetAll()
+                .Include(x => x.TimeSlot)
+                .Where(x => x.YogaClassId == existEnroll.YogaClassId)
+                .ToList().Select(x => x.TimeSlotId);
+
             existEnroll.IsActive = false;
             await _tcErRepo.UpdateAsync(existEnroll);
+
+            foreach (var timeSlot in classSchedule)
+            {
+                var teacherTimeSlot = tcSchedule
+                    .SingleOrDefault(x => x.TimeSlotId == timeSlot);
+                teacherTimeSlot.IsTaken = false;
+                await _tcScheduleRepo.UpdateAsync(teacherTimeSlot);
+            }
             return NoContent();
         }
         catch (Exception ex)
